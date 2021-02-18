eafca02391a9d73e0d84016ca2654b128636e807
fatmachihaouiLinedata <fatma.chihaoui@se.linedata.com>
2021-02-18 11:12:55 +0100

    first commit

---
+++ LinedataTrainingEventStore.SalesEventStore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinedataTrainingEventStore.SalesEventStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
